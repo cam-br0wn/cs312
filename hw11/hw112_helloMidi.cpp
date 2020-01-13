8487e75dbffc2ecd7a5db08a8a771e3ccc7db90a
cam-br0wn <brownc2@carleton.edu>
2020-01-13 14:40:31 -0600

    modified hw112

--- hw11/hw112_helloMidi.cpp
+++ hw11/hw112_helloMidi.cpp
@@ -2,11 +2,12 @@
 #include <iostream>
 #include <string>
 
+const char kTAB = '\t';
 // write_comment()
 // prints lines that start with # followed by string s
 // string s does not contain the "# " (pound space) to start the line
 void write_comment(const std::string &s) {
-  std::cout << "  write_comment()\t\t// you write" << std::endl;
+  std::cout << "# " << &s << std::endl;
 }
 
 // write_midi_message_nodata2(...)
@@ -15,7 +16,7 @@ void write_comment(const std::string &s) {
 void write_midi_message_nodata2(int32_t time_stamp, int16_t status,
                                 int16_t data1) 
 {
-  std::cout << "  write_midi_message_nodata2()\t// you write" << std::endl;
+  std::cout << time_stamp << kTAB << status << kTAB << data1 << std::endl;
 }
 
 // write_midi_message_hasdata2(...)
@@ -30,12 +31,21 @@ void write_midi_message_hasdata2(unsigned int timeStamp, unsigned char status,
 // write formatted output exactly as shown below
 // use the three functions you implemented above
 int main() {
-  std::cout << "If line begins with # use" << std::endl;
-  write_comment(" ");
+  write_comment("A MIDI message contains three or four numbers separated by a single tab character");
+  write_comment("number 1 is a time stamp in milliseconds in decimal format");
+  write_comment("number 2 is a MIDI status message in hexadecimal format without the 0x prefix (range 0x80 - 0xFF)");
+  write_comment("number 3 is a MIDI data1 message in decimal format (range 0 - 127)");
+  write_comment("number 4 is a MIDI data2 message in decimal format (range 0 - 127)");
+  std::cout << "\n" << std::endl;
+  write_comment("status 0xC0 begins a a patch change message");
+  write_comment("indicating which instrument sound to use");
+  write_comment("patch change messages do not use data2");
+  write_comment("data1 is the instrument number.");
+  write_midi_message_nodata2(0, 0xc0, 0);
 
-  std::cout << "\nIf midi message status is 0xC0 use" << std::endl;
-  write_midi_message_nodata2(0, 0, 0);
-
-  std::cout << "\nElse if midi message status is 0x80 or 0x90 use" << std::endl;
-  write_midi_message_hasdata2(0, 0, 0, 0);
+//  std::cout << "\nIf midi message status is 0xC0 use" << std::endl;
+//  write_midi_message_nodata2(0, 0, 0);
+//
+//  std::cout << "\nElse if midi message status is 0x80 or 0x90 use" << std::endl;
+//  write_midi_message_hasdata2(0, 0, 0, 0);
 }
---
+++ test_directory/tester.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Cam Brown on 1/13/20.
+//
+
+#include <iostream>
+
+int main(){
+    std::cout << "string" << std::endl;
+}
