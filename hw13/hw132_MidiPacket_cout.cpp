68953201c90e1e10fc3a2d8b1b640e8f25c33913
cam-br0wn <brownc2@carleton.edu>
2020-01-16 22:46:12 -0600

    hw13 update (thursday evening)

--- hw13/hw132_MidiPacket_cout.cpp
+++ hw13/hw132_MidiPacket_cout.cpp
@@ -6,16 +6,16 @@
 
 #include <iostream>
 
-MidiPacket2 create_midipacket2(uint32_t ts, uint8_t st, uint8_t d1){
-     MidiPacket2 *packet = new MidiPacket2();
-     packet->timestamp = ts;
-     packet->status = st;
-     packet->data1 = d1;
+MidiPacket2 create_midipacket2(int ts, int st, int d1){
+     MidiPacket2 packet = MidiPacket2();
+     packet.timestamp = ts;
+     packet.status = st;
+     packet.data1 = d1;
 
-     return *packet;
+     return packet;
 }
 
-MidiPacket2 create_midipacket2(uint32_t ts, uint8_t st, uint8_t d1, uint8_t d2){
+MidiPacket2 create_midipacket2(int ts, int st, int d1, int d2){
       MidiPacket2 *packet = new MidiPacket2();
       packet->timestamp = ts;
       packet->status = st;
@@ -27,32 +27,57 @@ MidiPacket2 create_midipacket2(uint32_t ts, uint8_t st, uint8_t d1, uint8_t d2){
 
 void print_midipacket2(const MidiPacket2 &mp){
  // print_size(mp);
-  std::cout << mp.timestamp << '\t' << mp.status << '\t' << mp.data1 << '\t' <<  std::endl;
+  std::cout << std::dec << mp.timestamp << '\t' << std::hex << mp.status << '\t' << std::dec << mp.data1 << '\t' << std::dec << mp.data2 << std::endl;
 }
 
 MidiPacket6 create_midipacket6(uint32_t ts, uint16_t st, uint16_t d1){
+  MidiPacket6 *packet = new MidiPacket6();
+  packet->timestamp = ts;
+  packet->status = st;
+  packet->data1 = d1;
+
+  return *packet;
 }
 
 MidiPacket6 create_midipacket6(uint32_t ts, uint16_t st, uint16_t d1, uint16_t d2){
+  MidiPacket6 *packet = new MidiPacket6();
+  packet->timestamp = ts;
+  packet->status = st;
+  packet->data1 = d1;
+  packet->data2 = d2;
+
+  return *packet;
 }
 
 void print_midipacket6(const MidiPacket6 &mp){
-  // you write
+  // print_size(mp);
+  if(true){
+    std::cout << std::dec << mp.timestamp << '\t' << std::hex << mp.status << '\t' << std::dec << mp.data1 << '\t' << std::dec << mp.data2 << std::endl;
+  }
+  else{
+    std::cout << std::dec << mp.timestamp << '\t' << std::hex << mp.status << '\t' << std::dec << mp.data1 <<  std::endl;
+  }
 }
 
 int main() {
   // DO NOT CHANGE
   MidiPacket2 mp2 = create_midipacket2(0, 0xc0, 11);
+  std::cout << "MidiPacket2: ";
   print_midipacket2(mp2);
-//   mp2 = create_midipacket2(0, 0x90, 60, 100);
-//   print_midipacket2(mp2);
-//   mp2 = create_midipacket2(1000, 0x80, 60, 0);
-//   print_midipacket2(mp2);
+  std::cout << "MidiPacket2: ";
+  mp2 = create_midipacket2(0, 0x90, 60, 100);
+  print_midipacket2(mp2);
+  std::cout << "MidiPacket2: ";
+  mp2 = create_midipacket2(1000, 0x80, 60, 0);
+  print_midipacket2(mp2);
 
-//   MidiPacket6 mp6 = create_midipacket6(0, 0xc0, 11);
-//   print_midipacket6(mp6);
-//   mp6 = create_midipacket6(0, 0x90, 60, 100);
-//   print_midipacket6(mp6);
-//   mp6 = create_midipacket6(1000, 0x80, 60, 0);
-//   print_midipacket6(mp6);
+  MidiPacket6 mp6 = create_midipacket6(0, 0xc0, 11);
+  std::cout << "MidiPacket6: ";
+  print_midipacket6(mp6);
+  std::cout << "MidiPacket6: ";
+  mp6 = create_midipacket6(0, 0x90, 60, 100);
+  print_midipacket6(mp6);
+  std::cout << "MidiPacket6: ";
+  mp6 = create_midipacket6(1000, 0x80, 60, 0);
+  print_midipacket6(mp6);
 }
