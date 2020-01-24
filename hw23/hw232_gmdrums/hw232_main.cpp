28e7417ad806e375abff3497660cca2e44517ed4
cam-br0wn <55596263+cam-br0wn@users.noreply.github.com>
2020-01-24 12:59:24 -0600

    Update hw232_main.cpp

--- hw23/hw232_gmdrums/hw232_main.cpp
+++ hw23/hw232_gmdrums/hw232_main.cpp
@@ -7,6 +7,11 @@ void print_header()
 {
   std::cout << "Match Header line by line from web page.\n";
   std::cout << std::endl;
+  std::cout << "======================================================\n";
+  std::cout << "General MIDI drum kit note assignments on channel 10\n";
+  std::cout << "Use status 0x99 for NON\n"
+  std::cout << "Use status 0x99 and velocity of 0 for NOF\n"
+  std::cout << "======================================================\n"
 }
 
 int main() {
