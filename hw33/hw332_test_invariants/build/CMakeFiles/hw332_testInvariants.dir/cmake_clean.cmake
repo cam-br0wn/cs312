cc479dc3f414319ac625d8fa44d52d1c7c546823
cam-br0wn <brownc2@carleton.edu>
2020-01-31 12:30:16 -0600

    Getting started on hw332

---
+++ hw33/hw332_test_invariants/build/CMakeFiles/hw332_testInvariants.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw332_testInvariants.dir/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/common/hw332_CMidiPacket.cpp.o"
+  "CMakeFiles/hw332_testInvariants.dir/hw332_main.cpp.o"
+  "CMakeFiles/hw332_testInvariants.dir/hw332_testInvariants.cpp.o"
+  "hw332_testInvariants"
+  "hw332_testInvariants.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw332_testInvariants.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
