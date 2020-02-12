b0ff83dac596a4978d8940b522f905736828d93d
cam-br0wn <brownc2@carleton.edu>
2020-02-12 00:49:26 -0600

    hw43 and hw51

---
+++ hw51/hw511_blues/build/CMakeFiles/blues51.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/common/hw332_CMidiPacket.cpp" "/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/cs312/hw51/hw511_blues/build/CMakeFiles/blues51.dir/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/common/hw332_CMidiPacket.cpp.o"
+  "/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/common/hw411_rand_int.cpp" "/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/cs312/hw51/hw511_blues/build/CMakeFiles/blues51.dir/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/common/hw411_rand_int.cpp.o"
+  "/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/common/hw421_CDelayMs.cpp" "/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/cs312/hw51/hw511_blues/build/CMakeFiles/blues51.dir/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/common/hw421_CDelayMs.cpp.o"
+  "/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/common/hw422_CAppleMidiSynth.cpp" "/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/cs312/hw51/hw511_blues/build/CMakeFiles/blues51.dir/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/common/hw422_CAppleMidiSynth.cpp.o"
+  "/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/common/hw423_CMidiTrack.cpp" "/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/cs312/hw51/hw511_blues/build/CMakeFiles/blues51.dir/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/common/hw423_CMidiTrack.cpp.o"
+  "/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/common/hw511_CInstrument.cpp" "/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/cs312/hw51/hw511_blues/build/CMakeFiles/blues51.dir/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/common/hw511_CInstrument.cpp.o"
+  "/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/cs312/hw51/hw511_blues/c513_CMarkovBlues.cpp" "/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/cs312/hw51/hw511_blues/build/CMakeFiles/blues51.dir/c513_CMarkovBlues.cpp.o"
+  "/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/cs312/hw51/hw511_blues/hw431_CDrumMachine.cpp" "/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/cs312/hw51/hw511_blues/build/CMakeFiles/blues51.dir/hw431_CDrumMachine.cpp.o"
+  "/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/cs312/hw51/hw511_blues/hw431_CDrumTrack.cpp" "/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/cs312/hw51/hw511_blues/build/CMakeFiles/blues51.dir/hw431_CDrumTrack.cpp.o"
+  "/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/cs312/hw51/hw511_blues/hw511_CBluesBassTrack.cpp" "/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/cs312/hw51/hw511_blues/build/CMakeFiles/blues51.dir/hw511_CBluesBassTrack.cpp.o"
+  "/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/cs312/hw51/hw511_blues/hw511_CBluesMelodyTrack.cpp" "/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/cs312/hw51/hw511_blues/build/CMakeFiles/blues51.dir/hw511_CBluesMelodyTrack.cpp.o"
+  "/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/cs312/hw51/hw511_blues/hw511_CBluesPianoTrack.cpp" "/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/cs312/hw51/hw511_blues/build/CMakeFiles/blues51.dir/hw511_CBluesPianoTrack.cpp.o"
+  "/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/cs312/hw51/hw511_blues/hw511_main.cpp" "/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/cs312/hw51/hw511_blues/build/CMakeFiles/blues51.dir/hw511_main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/common"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ hw51/hw511_blues/build/CMakeFiles/blues51.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/blues51.dir/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/common/hw332_CMidiPacket.cpp.o"
+  "CMakeFiles/blues51.dir/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/common/hw411_rand_int.cpp.o"
+  "CMakeFiles/blues51.dir/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/common/hw421_CDelayMs.cpp.o"
+  "CMakeFiles/blues51.dir/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/common/hw422_CAppleMidiSynth.cpp.o"
+  "CMakeFiles/blues51.dir/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/common/hw423_CMidiTrack.cpp.o"
+  "CMakeFiles/blues51.dir/Volumes/COURSES/cs312-00-w20/StuWork/brownc2/common/hw511_CInstrument.cpp.o"
+  "CMakeFiles/blues51.dir/c513_CMarkovBlues.cpp.o"
+  "CMakeFiles/blues51.dir/hw431_CDrumMachine.cpp.o"
+  "CMakeFiles/blues51.dir/hw431_CDrumTrack.cpp.o"
+  "CMakeFiles/blues51.dir/hw511_CBluesBassTrack.cpp.o"
+  "CMakeFiles/blues51.dir/hw511_CBluesMelodyTrack.cpp.o"
+  "CMakeFiles/blues51.dir/hw511_CBluesPianoTrack.cpp.o"
+  "CMakeFiles/blues51.dir/hw511_main.cpp.o"
+  "blues51"
+  "blues51.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/blues51.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
